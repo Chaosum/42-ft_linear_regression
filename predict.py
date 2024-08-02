98dd1f6cdddd2f527b6230dbb2df269970faff5a
Matthieu Servagent <mservage@z3r6p4.42lyon.fr>
2024-08-02 14:43:58 +0200

    fix try catch

--- predict.py
+++ predict.py
@@ -1,8 +1,8 @@
 def main():
     theta0 = 0
     theta1 = 0
-    meanMileage = 0
-    stdMileage = 0
+    meanMileage = 1
+    stdMileage = 1
     try:
         with open("gradient.csv", 'r') as gradients:
             next(gradients)  # Skip header
@@ -10,11 +10,13 @@ def main():
     except FileNotFoundError:
         print("gradient file not found : defalt value for theta0 and theta1")
     mileage = float(input("Enter the km of the car : "))
-    normalizedMileage = (mileage - meanMileage) / stdMileage  # Normalize the input mileage
-    predicted_price = theta0 + theta1 * normalizedMileage
-    if predicted_price < 0:
-        predicted_price = 0
-    print(f"The price of a car with {mileage} km is {predicted_price:.2f} euros.")
-
+    try:
+        normalizedMileage = (mileage - meanMileage) / stdMileage  # Normalize the input mileage
+        predicted_price = theta0 + theta1 * normalizedMileage
+        if predicted_price < 0:
+            predicted_price = 0
+        print(f"The price of a car with {mileage} km is {predicted_price:.2f} euros.")
+    except Exception as e:
+        print(f"Error : {e}")
 if __name__ == "__main__":
     main()
